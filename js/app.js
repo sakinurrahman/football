bd859efe3eac1949326408acc7084d14fd0db3fa
sakinurrahman <sakinu.ws99@gmail.com>
2021-08-06 22:49:17 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.getElementById('open-icon').addEventListener('click', function(){
+    document.getElementById('mob-menu').classList.add('mobile-menu-hide');
+});
+
+
+document.getElementById('close').addEventListener('click', function(){
+    document.getElementById('mob-menu').classList.remove('mobile-menu-hide');
+});
